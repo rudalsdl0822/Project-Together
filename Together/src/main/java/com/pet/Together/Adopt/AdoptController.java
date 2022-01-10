806971fc7856d4eaabdbef1b8b5f95127943821e
kyung186 <89821357+kyung186@users.noreply.github.com>
2022-01-10 16:21:17 +0900

    Bo (#43)
    * 테스트 최종
    
    입니다..
    
    * 테스트 최최종
    
    입니다..
    
    * 댓글/대댓글 파트
    
    확인바랍니다!
    
    Co-authored-by: rudalsdl0822 <89821065+rudalsdl0822@users.noreply.github.com>

--- Together/src/main/java/com/pet/Together/Adopt/AdoptController.java
+++ Together/src/main/java/com/pet/Together/Adopt/AdoptController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.pet.Together.AddPet.AddPetService;
 import com.pet.Together.AddPet.Pet;
 import com.pet.Together.Member.MemberService;
+import com.pet.Together.Reply.Reply;
 import com.pet.Together.Reply.ReplyService;
 
 @Controller
@@ -43,8 +44,11 @@ public class AdoptController {
 		ModelAndView mav=new ModelAndView("Adopt/WaitingPet","pet",p);
 		
 		/* ====댓글리스트 보경이파트========================= */
-		//ArrayList<Reply> reply_list=reply_service.getReplyListByPet_id(id);
-		//mav.addObject("replys", reply_list);
+		ArrayList<Reply> reply_list=reply_service.getReplyListByBoard_num(id);
+		mav.addObject("replys", reply_list);
+
+		ArrayList<Reply> childReply_list = reply_service.getListByParent_reply_num(id);
+		mav.addObject("c_replys", childReply_list);
 		/* ====댓글리스트 보경이파트 끝========================= */
 		
 		System.out.println("-----입양공고 상세보기------------------------------");
--- Together/src/main/java/com/pet/Together/Reply/ReplyService.java
+++ Together/src/main/java/com/pet/Together/Reply/ReplyService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.pet.Together.Member.MemberService;
 
-
 @Service
 public class ReplyService {
 
@@ -22,28 +21,28 @@ public class ReplyService {
 		r.setReply_num(mapper.selectSeqCurrval());
 	}
 
-// 댓글 내용 가져오기
+// 댓글내용 가져오기
 	public Reply getReply(int reply_num) {
-//		한개의 댓글 내용
+//		한개의 댓글내용
 		Reply r = mapper.select(reply_num);
-//		댓글의 자식 댓글 목록
+//		대댓글 목록
 		ArrayList<Reply> replyList = getListByParent_reply_num(reply_num);
 		for (int i = 0; i < replyList.size(); i++) {
-			Reply rReply = replyList.get(i);
-			rReply.setMember(memberService.getMember(rReply.getWriter_id()));
-			replyList.set(i, rReply);
+			Reply cReply = replyList.get(i);
+			cReply.setMember(memberService.getMember(cReply.getWriter_id()));
+			replyList.set(i, cReply);
 		}
 		r.setChild_reply(replyList);
 		r.setMember(memberService.getMember(r.getWriter_id()));
 		return r;
 	}
 
-//	게시판번호 댓글 가져오기
-	public ArrayList<Reply> getReplyListByPet_id(int board_num) {
+//	게시글 번호 댓글 가져오기
+	public ArrayList<Reply> getReplyListByBoard_num(int board_num) {
 		ArrayList<Reply> list = mapper.selectListByBoard_num(board_num);
 		for (int i = 0; i < list.size(); i++) {
 			list.set(i, getReply(list.get(i).getReply_num()));
-//			부모가 없는 댓글만 리스트에 넣는다.
+//			부모가 없는 댓글만 리스트에 넣음
 			if (list.get(i).getParent_reply_num() != -1) {
 				list.remove(i);
 				i--;
@@ -52,13 +51,15 @@ public class ReplyService {
 		return list;
 	}
 
-//	작성자의 댓글 목록
+//	작성자의 댓글목록
 	public ArrayList<Reply> getReplyListByWriter_id(String writer_id) {
 		return mapper.selectListByWriter_id(writer_id);
 	}
 
-//	댓글의 하위 댓글만 가져오기
-	private ArrayList<Reply> getListByParent_reply_num(int parent_reply_num) {
+//	대댓글만 가져오기
+	public ArrayList<Reply> getListByParent_reply_num(int parent_reply_num) {
+// private에서 public으로 수정
+
 		return mapper.selectListByParent_reply_num(parent_reply_num);
 	}
 
@@ -76,7 +77,7 @@ public class ReplyService {
 		mapper.deleteParent(reply_num);
 	}
 
-	// 현재 번호
+// 현재 번호
 	public int getSeqCurrval() {
 		return mapper.selectSeqCurrval();
 	}
